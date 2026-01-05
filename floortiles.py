7d26fae7f6b51f6d1addfeb4873fc5137247c2cf
katallen405 <95441393+katallen405@users.noreply.github.com>
2026-01-04 19:54:36 -0500

    fixed bug in edge generation logic
    Also fixed the tiles to match what I actually have

--- floortiles.py
+++ floortiles.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random
 import matplotlib.pyplot as plt
 # with some debugging help from Claude
-
-def draw_tile_map(tiles, grid_cols=5):
+n=8
+def draw_tile_map(colormap, grid_cols=n):
     colors = ["black", "green", "red", "blue", "grey", "beige"]
     
-    grid_rows = (len(tiles) + grid_cols - 1) // grid_cols
+    grid_rows = (len(colormap) + grid_cols - 1) // grid_cols
     tile_size = 1
     
     fig, ax = plt.subplots(figsize=(grid_cols * 2, grid_rows * 2))
     
-    for index, tile in enumerate(tiles):
+    for index, tile in enumerate(colormap):
         row = index // grid_cols
         col = index % grid_cols
         is_vertical = (row + col) % 2 == 1  # Tile 0 is horizontal
@@ -32,7 +32,7 @@ def draw_tile_map(tiles, grid_cols=5):
             # Horizontal bars
             for i, color_idx in enumerate(tile):
                 ax.add_patch(plt.Rectangle(
-                    (x, y + i*tile_size/4), 
+                    (x, y + (3-i)*tile_size/4),  # Changed: (3-i) instead of i
                     tile_size, tile_size/4, 
                     color=colors[color_idx]
                 ))
@@ -42,6 +42,7 @@ def draw_tile_map(tiles, grid_cols=5):
     ax.set_aspect('equal')
     ax.axis('off')
     plt.tight_layout()
+    plt.savefig("tile_map.png")
     plt.show()
 
 
@@ -62,76 +63,55 @@ def issafe(vertex, tile, edges_dict, colormap):
     
     # Check all_to_first: all 4 bars of current tile touch first bar of neighbor
     for edge in edges_dict['all_to_first']:
-        if edge[0] == vertex:  # Current vertex is source
-            neighbor = edge[1]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                neighbor_first_bar = colormap[neighbor][0]
-                # Check if any of our 4 bars matches neighbor's first bar
-                if neighbor_first_bar in tile:
-                    return False
-        elif edge[1] == vertex:  # Current vertex is target
-            neighbor = edge[0]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                our_first_bar = tile[0]
-                # Check if our first bar matches any of neighbor's 4 bars
-                if our_first_bar in colormap[neighbor]:
-                    return False
-    
+        if edge[0] != vertex:
+            continue
+        neighbor = edge[1]
+        if colormap[neighbor] != 0:  # Neighbor is assigned
+            neighbor_first_bar = colormap[neighbor][0]
+            # Check if any of our 4 bars matches neighbor's first bar
+            if neighbor_first_bar in tile:
+                print("tile", tile, "bars conflict with neighbor", neighbor, "first bar", colormap[neighbor])
+                return False
+      
     # Check first_to_all: first bar of current tile touches all 4 bars of neighbor
     for edge in edges_dict['first_to_all']:
-        if edge[0] == vertex:  # Current vertex is source
-            neighbor = edge[1]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                our_first_bar = tile[0]
-                # Check if our first bar matches any of neighbor's 4 bars
-                if our_first_bar in colormap[neighbor]:
-                    return False
-        elif edge[1] == vertex:  # Current vertex is target
-            neighbor = edge[0]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                neighbor_first_bar = colormap[neighbor][0]
-                # Check if neighbor's first bar matches any of our 4 bars
-                if neighbor_first_bar in tile:
-                    return False
+        if edge[0] != vertex:
+            continue
+        neighbor = edge[1]
+        if colormap[neighbor] != 0:  # Neighbor is assigned
+            our_first_bar = tile[0]
+            # Check if our first bar matches any of neighbor's 4 bars
+            if our_first_bar in colormap[neighbor]:
+                print("tile", tile, "first bar conflicts with neighbor", neighbor, "bars", colormap[neighbor])
+                return False
     
     # Check all_to_last: all 4 bars of current tile touch last bar of neighbor
     for edge in edges_dict['all_to_last']:
-        if edge[0] == vertex:  # Current vertex is source
-            neighbor = edge[1]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                neighbor_last_bar = colormap[neighbor][3]
-                # Check if any of our 4 bars matches neighbor's last bar
-                if neighbor_last_bar in tile:
-                    return False
-        elif edge[1] == vertex:  # Current vertex is target
-            neighbor = edge[0]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                our_last_bar = tile[3]
-                # Check if our last bar matches any of neighbor's 4 bars
-                if our_last_bar in colormap[neighbor]:
-                    return False
-    
+        if edge[0] != vertex:
+            continue
+        neighbor = edge[1]
+        if colormap[neighbor] != 0:  # Neighbor is assigned
+            neighbor_last_bar = colormap[neighbor][3]
+            # Check if any of our 4 bars matches neighbor's last bar
+            if neighbor_last_bar in tile:
+                print("tile", tile, "conflicts with neighbor", neighbor, "last bar", colormap[neighbor])
+                return False
+
     # Check last_to_all: last bar of current tile touches all 4 bars of neighbor
     for edge in edges_dict['last_to_all']:
-        if edge[0] == vertex:  # Current vertex is source
-            neighbor = edge[1]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                our_last_bar = tile[3]
-                # Check if our last bar matches any of neighbor's 4 bars
-                if our_last_bar in colormap[neighbor]:
-                    return False
-        elif edge[1] == vertex:  # Current vertex is target
-            neighbor = edge[0]
-            if colormap[neighbor] != 0:  # Neighbor is assigned
-                neighbor_last_bar = colormap[neighbor][3]
-                # Check if neighbor's last bar matches any of our 4 bars
-                if neighbor_last_bar in tile:
-                    return False
+        if edge[0] != vertex:
+            continue
+        neighbor = edge[1]
+        if colormap[neighbor] != 0:  # Neighbor is assigned
+            our_last_bar = tile[3]
+            # Check if our last bar matches any of neighbor's 4 bars
+            if our_last_bar in colormap[neighbor]:
+                print("tile", tile, "last bar conflicts with neighbor", neighbor, "bars", colormap[neighbor])
+                return False
     
     return True
 
 
-# Example usage with backtracking:
 def solve_coloring(n, tiles_list, edges_dict):
     """
     Solve the tile coloring problem using backtracking.
@@ -145,27 +125,44 @@ def solve_coloring(n, tiles_list, edges_dict):
         colormap if solution found, None otherwise
     """
     num_tiles = n * n
+    print("Number of tiles is ", num_tiles)
     colormap = [0] * num_tiles
+    counter = 0  # Counter for tiles tried at vertex 0
     
-    def backtrack(vertex):
-        if vertex == num_tiles:
-            return True  # All tiles assigned successfully
-        
-        for tile in tiles_list:
-            if issafe(vertex, tile, edges_dict, colormap):
-                colormap[vertex] = tile
-                
-                if backtrack(vertex + 1):
-                    return True
-                
-                colormap[vertex] = 0  # Backtrack
-        
-        return False
-    
-    if backtrack(0):
-        return colormap
-    else:
-        return None
+    for tile in tiles_list:
+        counter = counter + 1
+        print("Trying origin tile", tile, "at vertex 0", counter, "of", len(tiles_list))
+        colormap[0] = tile
+        if backtrack(1, num_tiles, tiles_list, edges_dict, colormap):
+            return colormap
+        else:
+            print("Tile", tile, "does not work at vertex 0, trying next tile")
+            colormap[0] = 0  # Backtrack
+
+
+    print("Could not solve colormap")
+    return None
+
+def backtrack(vertex, num_tiles, tiles_list, edges_dict, colormap):
+    if vertex == num_tiles:
+        print("All tiles assigned successfully")
+        return True  # All tiles assigned successfully
+    counter = 0
+    for tile in tiles_list:
+        counter = counter + 1
+        print("Trying tile", tile, "at vertex", vertex, "with colormap", colormap, "tile number", counter, "of", len(tiles_list))
+        if issafe(vertex, tile, edges_dict, colormap):
+            colormap[vertex] = tile
+            print("Placing tile", tile,  "at vertex", vertex)
+            if backtrack(vertex + 1, num_tiles, tiles_list, edges_dict, colormap):
+                print("Tile", tile, "works at vertex", vertex)
+                return True
+        else:
+            print("Tile", tile, "does not work at vertex", vertex)
+            colormap[vertex] = 0
+
+
+    return False
 
 def generate_grid_edges(n):
     """
@@ -238,14 +235,17 @@ def generate_grid_edges(n):
                 if row - 1 >= 0:
                     neighbor = node - n
                     # Last bar of horizontal touches all bars of vertical (above)
-                    last_to_all.append([node, neighbor])
+                    first_to_all.append([node, neighbor])
                 
                 # Bottom neighbor (vertical tile)
                 if row + 1 < n:
                     neighbor = node + n
                     # First bar of horizontal touches all bars of vertical (below)
-                    first_to_all.append([node, neighbor])
-    
+                    last_to_all.append([node, neighbor])
+    print("all_to_first:", all_to_first)
+    print("first_to_all:", first_to_all)
+    print("all_to_last:", all_to_last)
+    print("last_to_all:", last_to_all)
     return {
         'all_to_first': all_to_first,
         'first_to_all': first_to_all,
@@ -253,9 +253,28 @@ def generate_grid_edges(n):
         'last_to_all': last_to_all
     }
 
-    
-tiles = [[3,1,4,5],[5,4,1,3],[3,1,4,2],[2,4,1,3],[3,2,4,1],[1,4,2,3],[4,2,1,3],[3,1,2,4],[2,3,1,4],[4,1,3,2],[2,3,0,4],[4,0,3,2],[2,0,1,5],[5,1,0,2],[0,1,2,5],[5,2,1,0],[2,4,1,3],[3,1,4,2],[3,1,2,5],[5,2,1,3],[3,1,4,5],[5,4,1,3],[3,0,1,5],[5,1,0,3]]
-    
-edges = generate_grid_edges(10)
-colormap = solve_coloring(10, tiles, edges)
+# colors = ["black", "green", "red", "blue", "grey", "beige"] as [0 1 2 3 4 5]   
+#tiles = [[3,1,4,5],[5,4,1,3],[3,1,4,2],[2,4,1,3],[3,2,4,1],[1,4,2,3],[4,2,1,3],[3,1,2,4],[2,3,1,4],[4,1,3,2],[2,3,0,4],[4,0,3,2],[2,0,1,5],[5,1,0,2],[0,1,2,5],[5,2,1,0],[2,4,1,3],[3,1,4,2],[3,1,2,5],[5,2,1,3],[3,1,4,5],[5,4,1,3],[3,0,1,5],[5,1,0,3]]
+tiles = [[2,4,1,3],
+         [3,1,4,5],
+         [5,1,0,2],
+         [0,1,2,5],
+         [5,2,1,0],
+         [2,4,1,3],
+         [3,1,4,2],
+         [3,1,2,5],
+         [5,2,1,3],
+         [3,1,4,5],
+         [5,4,1,3],
+         [3,0,1,5],
+         [5,1,0,3],
+         [3,1,2,5],
+         [3,0,1,5],
+         [2,0,1,5]]
+random.shuffle(tiles)
+print(tiles)
+edges = generate_grid_edges(n)
+colormap = solve_coloring(n, tiles, edges)
+print("colormap is ", colormap)
 draw_tile_map(colormap)
+
